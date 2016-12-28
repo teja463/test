d799b83a8fe91b6f01c30fe12f0e02b6572a8c82
Teja P <teja463@gmail.com>
2016-12-28 18:30:58 +0530

    adding test cases, propetis files and main class

---
+++ src/test/java/com/teja/test/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.teja.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
