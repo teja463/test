da43a778880f64e0218346d2be420499c2c5241b
Teja P <teja463@gmail.com>
2017-01-05 13:05:54 +0530

    Adding Hello World file

---
+++ TestProject/src/com/test/Sample1.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		System.out.println("Main Hello World..");
+		for(int i=0; i<5 ;i++){
+			System.out.println("Hello World..");
+		}
+	}
+
+}
